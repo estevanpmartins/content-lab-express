047af8e516dd4f907cae52b816d78dfb6020aa32
estevanpmartins <estevanpmartins@gmail.com>
2021-08-16 07:39:29 -0400

    Monday Commit

--- javascript.js
+++ javascript.js
@@ -7,9 +7,23 @@ $(document).ready(function() {
     });
 });
 
+
 function foo() {
     if ( $(submitBtn).hasClass('active') )  {
         window.location.href = "thankyou.html";
     }
     setTimeout(foo, 3000);
 }
+
+
+$(document).ready(function() {
+/* Image Swap On Hover */
+document.getElementsByClassName("nav-dev").addEventListener("mouseover", function() {
+    document.getElementsByClassName("nav-dev").css("display","block");
+  }, false);
+
+  document.getElementsByClassName("nav-dev").addEventListener("mouseout", function() {
+     document.getElementsByClassName("nav-dev").css("display","none");
+  }, false);
+
+});
