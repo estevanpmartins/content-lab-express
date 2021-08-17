0912ad16aa0f2d6dbcf0c020bdec57cca83b4cb4
estevanpmartins <estevanpmartins@gmail.com>
2021-08-17 00:34:39 -0400

    last commit

--- javascript.js
+++ javascript.js
@@ -14,16 +14,3 @@ function foo() {
     }
     setTimeout(foo, 3000);
 }
-
-
-$(document).ready(function() {
-/* Image Swap On Hover */
-document.getElementsByClassName("nav-dev").addEventListener("mouseover", function() {
-    document.getElementsByClassName("nav-dev").css("display","block");
-  }, false);
-
-  document.getElementsByClassName("nav-dev").addEventListener("mouseout", function() {
-     document.getElementsByClassName("nav-dev").css("display","none");
-  }, false);
-
-});
